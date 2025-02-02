511d146c251e059dfe855b8e339d8101c3672ff2
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-02-02 22:53:31 +0100

    sql

--- task_6.sql
+++ task_6.sql
@@ -1,2 +1,4 @@
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave.");
+VALUES (2, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave.");
+VALUES (3, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
