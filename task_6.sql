57f0865d9a5f1ea999e64b59fddf51df73627e28
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-02-02 20:28:19 +0100

    sql

--- task_6.sql
+++ task_6.sql
@@ -1,10 +1,15 @@
 -- task_6.sql
 
--- Use the database passed as an argument
-USE alx_book_store;
+-- Use the database specified as an argument.  This assumes the mysql command
+-- will be called like:  mysql -u <user> -p <database_name> < task_6.sql
+USE `$1`;  -- $1 will be replaced with the database name argument.
 
--- Insert multiple rows into the customer table
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', " 124 Happiness Ave. "),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Insert multiple rows into the customer table.
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+
+-- Verify the insertion (optional, but good practice).
+SELECT * FROM customer WHERE customer_id IN (2, 3, 4);
