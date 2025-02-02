c11e3df6d211c5990ebc9c21ed37f56bbe561964
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-02-02 20:19:48 +0100

    sql

--- task_6.sql
+++ task_6.sql
@@ -1,3 +1,9 @@
+-- task_6.sql
+
+-- Use the database passed as an argument
+USE alx_book_store;
+
+-- Insert multiple rows into the customer table
 INSERT INTO customer (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
