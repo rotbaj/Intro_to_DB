5afc5fd120568b18e038826f7dd1755c7b04482f
rotbaj <109310264+rotbaj@users.noreply.github.com>
2025-02-02 20:05:08 +0100

    sql

--- MySQLServer.py
+++ MySQLServer.py
@@ -19,7 +19,7 @@ def create_database():
             # Print success message
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         # Print error message if connection or creation fails
         print(f"Error: {e}")
 
